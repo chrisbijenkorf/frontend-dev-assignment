a3ce8ac3572cdf3f30592030e2750cd7eeb5ca91
bridgetcuevas <bridgetmichele@gmail.com>
2019-08-16 13:48:46 +0200

    install ionicons/add search icon

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import IosSearch from "../../node_modules/react-ionicons/lib/IosSearch";
 
 const SearchForm = () => {
   return (
@@ -10,7 +11,9 @@ const SearchForm = () => {
         placeholder="Zoeken"
       />
 
-      <button className="search-button" type="submit" />
+      <button className="search-button" type="submit">
+        <IosSearch />
+      </button>
     </form>
   );
 };
