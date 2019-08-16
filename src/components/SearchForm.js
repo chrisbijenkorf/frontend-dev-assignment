5ccdd945a8e44126ac19d776e2c4d99eae272f4d
bridgetcuevas <bridgetmichele@gmail.com>
2019-08-16 19:03:24 +0200

    add search form css

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,25 @@
 import React from "react";
 import IosSearch from "../../node_modules/react-ionicons/lib/IosSearch";
+import "../css/SearchForm.css";
 
 const SearchForm = () => {
   return (
-    <form action="" className="search-form" data-at="SearchForm">
-      <input
-        type="search"
-        name="query"
-        className="search-input"
-        placeholder="Zoeken"
-      />
-
-      <button className="search-button" type="submit">
-        <IosSearch />
-      </button>
+    <form action="" className="search-form">
+      <div className="form-element">
+        <input
+          autoComplete="off"
+          autocapitalize="off"
+          autocorrect="off"
+          spellcheck="off"
+          type="search"
+          name="query"
+          className="search-input"
+          placeholder="Zoeken"
+        />
+        <button className="search-button" type="submit">
+          <IosSearch color="#3e3e3e" fontSize="17px" />
+        </button>
+      </div>
     </form>
   );
 };
