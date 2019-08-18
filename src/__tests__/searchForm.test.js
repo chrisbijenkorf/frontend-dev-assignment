0e7cd011837d4e1a93b4f08a67b275b2d3bd5d8a
bridgetcuevas <bridgetmichele@gmail.com>
2019-08-18 13:55:38 +0200

    add search input test

--- src/__tests__/searchForm.test.js
+++ src/__tests__/searchForm.test.js
@@ -1,11 +1,23 @@
 import React from "react";
 import SearchForm from "../components/searchForm";
 import toJSON from "enzyme-to-json";
-import { render } from "enzyme";
+import { render, mount } from "enzyme";
+
 
 describe("Search input component", () => {
+
   it("should match snapshot", () => {
     const wrapper = render(<SearchForm />);
+
     expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it("should render search input and search for sweater", () => {
+    const searchForm = mount(<SearchForm />);
+    const input = searchForm.find("input");
+    input.simulate("change", { target: { value: "sweater" } });
+    
+    expect(input).toHaveLength(1);
+    expect(input.prop("type")).toEqual("search");
   });
 });
